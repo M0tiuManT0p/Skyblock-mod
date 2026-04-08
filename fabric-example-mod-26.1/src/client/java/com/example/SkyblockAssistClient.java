95d66f4b60d45f73d963b7ed45f39c8c5efd60b4
M0tiuMant0p <ma.levchenko@ukma.edu.ua>
2026-04-08 15:08:55 +0300

    piva

---
+++ fabric-example-mod-26.1/src/client/java/com/example/SkyblockAssistClient.java
@@ -0,0 +1,117 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands; // <-- Change: ClientCommands instead of ClientCommandManager
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.fabricmc.loader.api.FabricLoader;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.minecraft.network.chat.Component;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+public class SkyblockAssistClient implements ClientModInitializer {
+
+    public static SkyblockAssistConfig config;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[SkyblockAI] Mod is loading...");
+        loadConfig();
+
+        // We are using the most basic registration possible
+        net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal("assist")
+                    .executes(context -> {
+                        // Bare minimum logic: No arguments, no chat components
+                        System.out.println("[AI] Command triggered!");
+
+                        java.util.concurrent.CompletableFuture.runAsync(() -> {
+                            String ahPrice = fetchCoflnetAhPrice("RECOMBOBULATOR_3000");
+                            System.out.println("[API Results]: Recomb is " + ahPrice);
+                        });
+
+                        return 1;
+                    })
+            );
+        });
+    }
+
+    private void loadConfig() {
+        try {
+            File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "skyblock_assist.json");
+
+            if (configFile.exists()) {
+                String json = Files.readString(configFile.toPath());
+                config = GSON.fromJson(json, SkyblockAssistConfig.class);
+            } else {
+                config = new SkyblockAssistConfig();
+                Files.writeString(configFile.toPath(), GSON.toJson(config));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            config = new SkyblockAssistConfig();
+        }
+    }
+
+    private String fetchCoflnetAhPrice(String itemId) {
+        try {
+            String url = "https://sky.coflnet.com/api/item/price/" + itemId + "/bin";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Accept", "application/json")
+                    .header("User-Agent", "SkyblockAssistMod/1.0")
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
+                double lowestBin = json.get("lowest").getAsDouble();
+                return String.format("%,.0f coins", lowestBin);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Unknown AH Price";
+    }
+
+    private String fetchCoflnetBazaarPrice(String itemId) {
+        try {
+            String url = "https://sky.coflnet.com/api/bazaar/" + itemId + "/snapshot";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Accept", "application/json")
+                    .header("User-Agent", "SkyblockAssistMod/1.0")
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
+                double sellPrice = json.get("sellPrice").getAsDouble();
+                return String.format("%,.0f coins", sellPrice);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Unknown BZ Price";
+    }
+}
---
+++ fabric-example-mod-26.1/src/client/java/com/example/SkyblockAssistConfig.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class SkyblockAssistConfig {
+    // This is where you put your Gemini key later
+    public String geminiApiKey = "YOUR_API_KEY_HERE";
+
+    // Your prompt instructions for me
+    public String systemPrompt = "You are a Skyblock expert. Keep answers short, punchy, and without markdown formatting. Focus on the meta.";
+}
